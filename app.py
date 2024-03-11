76f85806236b313158740f73d2229c2d2266c3b6
HenriChap <henriquechapola@gmail.com>
2024-03-10 23:52:28 -0300

    Fix typo

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import sys
 from Bio import Entrez
 
 Entrez.email = sys.argv[1]
-handle = Entrez.efetch(db="nucleotide", id=sys.arv[2:], rettype="fasta")
+handle = Entrez.efetch(db="nucleotide", id=sys.argv[2:], rettype="fasta")
 records = handle.read
 print(records)
