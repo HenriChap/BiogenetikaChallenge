6d128de2ac7713f6ad41f64d95e402441ec66cfe
HenriChap <henriquechapola@gmail.com>
2024-03-11 00:09:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import sys
 from Bio import Entrez
+from Bio import SeqIO
 
+arguments = sys.argv[1:]
+if len(arguments) > 11:
+    print('Error: please, search for a maximum of 10 IDs')
+    sys.exit()
 Entrez.email = sys.argv[1]
 handle = Entrez.efetch(db="nucleotide", id=sys.argv[2:], rettype="fasta")
-records = handle.read
+records = handle.read()
 print(records)
